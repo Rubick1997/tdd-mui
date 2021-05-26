ac8d4277f906d4dacaf113f7c0aa9556bbfa2c54
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-05-26 13:51:09 -0700

    element to be removed

--- src/pages/SummaryForm.js
+++ src/pages/SummaryForm.js
@@ -1,18 +1,71 @@
 import React, { useState } from "react";
 import {
-  FormGroup,
   FormControlLabel,
   Checkbox,
   Button,
+  Typography,
+  Popover,
+  makeStyles
 } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
 
 function SummaryForm() {
   const [checked, setChecked] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = useStyles();
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}> Terms and Conditions</span>
+      I agree to
+      <span style={{ color: "blue" }}>
+        <Typography
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+        >
+          Terms and Conditions
+        </Typography>
+        <Popover
+          id="mouse-over-popover"
+          className={classes.popover}
+        classes={{
+          paper: classes.paper,
+        }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography>No ice cream will actually be delivered</Typography>
+        </Popover>
+      </span>
     </span>
   );
+
   return (
     <form action="">
       <FormControlLabel
@@ -29,7 +82,6 @@ function SummaryForm() {
         color="primary"
         variant="contained"
         disabled={!checked}
-        type="submit"
       >
         Confirm Order
       </Button>
--- src/pages/__tests__/SummaryForm.test.js
+++ src/pages/__tests__/SummaryForm.test.js
@@ -1,5 +1,11 @@
 import SummaryForm from "../SummaryForm";
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  queryByText,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("checkbox is unchecked by default", () => {
   render(<SummaryForm />);
@@ -18,8 +24,29 @@ test("checking box enables the button back", () => {
   });
   const button = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeEnabled();
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  //hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+  //appears upon mouseover
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
